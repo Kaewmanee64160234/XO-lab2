57b4bf99169952ef6274c1e7ce54e91be19f9ecb
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-07-12 13:17:13 +0700

    ✨ create project

---
+++ src/main/java/com/mycompany/superox/SuperOX.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.superox;
+
+/**
+ *
+ * @author USER
+ */
+public class SuperOX {
+public  static String[][] board = {{"-","-","-"},{"-","-","-"},{"-","-","-"}};
+public static  String turn ;
+public static String row;
+public static String column;
+public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
