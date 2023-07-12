763c460123281820558512ef20b138ae3f84e610
Kaewmanee Marasri <64160234@go.buu.ac.th>
2023-07-12 14:12:38 +0700

    📝 create func inputRowAndColumn()

--- src/main/java/com/mycompany/superox/SuperOX.java
+++ src/main/java/com/mycompany/superox/SuperOX.java
@@ -13,8 +13,8 @@ public class SuperOX {
 
     public static String[][] board = {{"-", "-", "-"}, {"-", "-", "-"}, {"-", "-", "-"}};
     public static String turn;
-    public static String row;
-    public static String column;
+    public static int row;
+    public static int column;
     public static boolean isEnd = false;
 
     public static void startGame() {
@@ -33,23 +33,54 @@ public class SuperOX {
             isEnd = false;
         }
     }
-    public static void showBoard(){
+
+    public static void showBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j]+" ");
-                
+                System.out.print(board[i][j] + " ");
+
             }
             System.out.println();
-            
+
         }
     }
 
-    public static void main(String[] args) {
-        startGame();
-        while (!isEnd) {
-            showBoard();
-            isEnd = true;
+    public static void showTurn() {
+        System.out.println("Turn>>> " + turn.toUpperCase());
+    }
+
+    public static void inputRowAndColumn() {
+        showTurn();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input Row :");
+        row = sc.nextInt();
+        System.out.print("Input Column :");
+        column = sc.nextInt();
+        if ((row > 0 && row < 4) && (column > 0 && column < 4)) {
+            while ((!(row > 0 && row < 4) && !(column > 0 && column < 4)) && !(board[row - 1][column - 1].equals("-"))) {
+                showTurn();
+                System.out.print("Input Row :");
+                row = sc.nextInt();
+                System.out.print("Input Column :");
+                column = sc.nextInt();
+            }
+            board[row - 1][column - 1] = turn;
+        } else {
+            return;
         }
-        System.out.println("end");
+
+    }
+
+    public static void main(String[] args) {
+        turn = "X";
+//        startGame();
+      
+        showBoard();
+        while (!isEnd) {
+           inputRowAndColumn();
+             showBoard();
+            
+        }
+//        System.out.println("end");
     }
 }
